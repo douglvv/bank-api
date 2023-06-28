3bba767b251a20861a339f16ec668601b5b2ea02
Douglas Vidal <vidaldl01@gmail.com>
2023-06-28 02:37:10 -0300

    update response transfer

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -240,7 +240,7 @@ module.exports = class AccountController {
             await payer.save();
             await receiver.save();
 
-            res.status(200).json({account: payer});
+            res.status(200).json(payer);
         } catch (error) {
             // console.log(error.message)
             res.status(500).send(error.message)
